da8cd5f6032ca516b57ec767921faa05787502be
Jordan Last <jordan.michael.last@gmail.com>
2017-07-04 16:44:35 -0600

    AST fully complete without testing!

--- assessml.ts
+++ assessml.ts
@@ -2,24 +2,26 @@ import {AST} from './typings/ast';
 import {Variable} from './typings/variable';
 import {Input} from './typings/input';
 import {Content} from './typings/content';
+import {Check} from './typings/check';
+import {Radio} from './typings/radio';
+import {Drag} from './typings/drag';
+import {Drop} from './typings/drop';
 
 export function generateAST(source: string) {
     return buildAST(source, {
         type: 'AST',
         ast: []
-    }, 0);
+    }, 0, 0, 0, 0, 0);
 }
 
-function buildAST(source: string, ast: AST, numInputs: number): AST {
-    console.log(source);
-
+function buildAST(source: string, ast: AST, numInputs: number, numChecks: number, numRadios: number, numDrags: number, numDrops: number): AST {
     const variableRegex: RegExp = /\[var(.+?)\]/;
     const inputRegex: RegExp = /\[input\]/;
     const checkRegex: RegExp = /\[x\](.+?)\[x\]/;
     const radioRegex: RegExp = /\[\*\](.+?)\[\*\]/;
     const dragRegex: RegExp = /\[drag\](.+?)\[drag\]/;
     const dropRegex: RegExp = /\[drop\](.+?)\[drop\]/;
-    const contentRegex: RegExp = new RegExp(`((.|\n)+?)((${variableRegex.source}|${inputRegex.source})|$)`);
+    const contentRegex: RegExp = new RegExp(`((.|\n)+?)((${variableRegex.source}|${inputRegex.source}|${checkRegex.source}|${radioRegex.source})|$)`);
 
     if (source.search(variableRegex) === 0) {
         const match = source.match(variableRegex) || [];
@@ -33,7 +35,7 @@ function buildAST(source: string, ast: AST, numInputs: number): AST {
         return buildAST(source.replace(matchedContent, ''), {
             ...ast,
             ast: [...ast.ast, variable]
-        }, numInputs);
+        }, numInputs, numChecks, numRadios, numDrags, numDrops);
     }
 
     if (source.search(inputRegex) === 0) {
@@ -47,7 +49,83 @@ function buildAST(source: string, ast: AST, numInputs: number): AST {
         return buildAST(source.replace(matchedContent, ''), {
             ...ast,
             ast: [...ast.ast, input]
-        }, numInputs + 1);
+        }, numInputs + 1, numChecks, numRadios, numDrags, numDrops);
+    }
+
+    if (source.search(checkRegex) === 0) {
+        const match = source.match(checkRegex) || [];
+        const matchedContent = match[0];
+        const insideContent = match[1];
+        const check: Check = {
+            type: 'CHECK',
+            varName: `check${numChecks + 1}`,
+            content: <(Content | Variable)[]> buildAST(insideContent, {
+                type: 'AST',
+                ast: []
+            }, 0, 0, 0, 0, 0).ast
+        };
+
+        return buildAST(source.replace(matchedContent, ''), {
+            ...ast,
+            ast: [...ast.ast, check]
+        }, numInputs, numChecks + 1, numRadios, numDrags, numDrops);
+    }
+
+    if (source.search(radioRegex) === 0) {
+        const match = source.match(radioRegex) || [];
+        const matchedContent = match[0];
+        const insideContent = match[1];
+        const radio: Radio = {
+            type: 'RADIO',
+            varName: `radio${numRadios + 1}`,
+            content: <(Content | Variable)[]> buildAST(insideContent, {
+                type: 'AST',
+                ast: []
+            }, 0, 0, 0, 0, 0).ast
+        };
+
+        return buildAST(source.replace(matchedContent, ''), {
+            ...ast,
+            ast: [...ast.ast, radio]
+        }, numInputs, numChecks, numRadios + 1, numDrags, numDrops);
+    }
+
+    if (source.search(dragRegex) === 0) {
+        const match = source.match(dragRegex) || [];
+        const matchedContent = match[0];
+        const insideContent = match[1];
+        const drag: Drag = {
+            type: 'DRAG',
+            varName: `drag${numDrags + 1}`,
+            content: <(Content | Variable)[]> buildAST(insideContent, {
+                type: 'AST',
+                ast: []
+            }, 0, 0, 0, 0, 0).ast
+        };
+
+        return buildAST(source.replace(matchedContent, ''), {
+            ...ast,
+            ast: [...ast.ast, drag]
+        }, numInputs, numChecks, numRadios, numDrags + 1, numDrops);
+    }
+
+    if (source.search(dropRegex) === 0) {
+        const match = source.match(dropRegex) || [];
+        const matchedContent = match[0];
+        const insideContent = match[1];
+        const drop: Drop = {
+            type: 'DROP',
+            varName: `drop${numDrops + 1}`,
+            content: <(Content | Variable)[]> buildAST(insideContent, {
+                type: 'AST',
+                ast: []
+            }, 0, 0, 0, 0, 0).ast
+        };
+
+        return buildAST(source.replace(matchedContent, ''), {
+            ...ast,
+            ast: [...ast.ast, drop]
+        }, numInputs, numChecks, numRadios, numDrags, numDrops + 1);
     }
 
     if (source.search(contentRegex) === 0) {
@@ -61,7 +139,7 @@ function buildAST(source: string, ast: AST, numInputs: number): AST {
         return buildAST(source.replace(matchedContent, ''), {
             ...ast,
             ast: [...ast.ast, content]
-        }, numInputs);
+        }, numInputs, numChecks, numRadios, numDrags, numDrops);
     }
 
     return ast;
--- test/assessml-test.ts
+++ test/assessml-test.ts
@@ -1,7 +1,14 @@
 import {generateAST} from '../assessml';
 
 const ast = generateAST(`
-    What [input] is [var1] + [var2] [input] [input] [input]?
+    What color is the sky?
+    [*] Blue [*]
+    [*] Red [*]
+    [*] Green [*]
+    [*] Yellow [*]
+
+
+    
 `);
 
 console.log(ast);
