9e5486abe25c321c75818cbeb7f626765e22813f
Jordan Last <jordan.michael.last@gmail.com>
2017-07-08 18:42:34 -0600

    release v0.0.9, typings exporting well

--- assessml.d.ts
+++ assessml.d.ts
@@ -1,51 +1,49 @@
-declare namespace AssessML {
-    interface AST {
-        type: 'AST';
-        ast: (Content | Variable | Input | Essay | Check | Radio | Drag | Drop)[];
-    }
+export interface AST {
+    type: 'AST';
+    ast: (Content | Variable | Input | Essay | Check | Radio | Drag | Drop)[];
+}
 
-    interface Check {
-        type: 'CHECK';
-        varName: string;
-        content: (Variable | Content)[];
-    }
+export interface Check {
+    type: 'CHECK';
+    varName: string;
+    content: (Variable | Content)[];
+}
 
-    interface Content {
-        type: 'CONTENT';
-        content: string;
-    }
+export interface Content {
+    type: 'CONTENT';
+    content: string;
+}
 
-    interface Drag {
-        type: 'DRAG';
-        varName: string;
-        content: (Variable | Content)[];
-    }
+export interface Drag {
+    type: 'DRAG';
+    varName: string;
+    content: (Variable | Content)[];
+}
 
-    interface Drop {
-        type: 'DROP';
-        varName: string;
-        content: (Variable | Content)[];
-    }
+export interface Drop {
+    type: 'DROP';
+    varName: string;
+    content: (Variable | Content)[];
+}
 
-    interface Essay {
-        type: 'ESSAY';
-        varName: string;
-    }
+export interface Essay {
+    type: 'ESSAY';
+    varName: string;
+}
 
-    interface Input {
-        type: 'INPUT';
-        varName: string;
-    }
+export interface Input {
+    type: 'INPUT';
+    varName: string;
+}
 
-    interface Radio {
-        type: 'RADIO';
-        varName: string;
-        content: (Variable | Content)[];
-    }
+export interface Radio {
+    type: 'RADIO';
+    varName: string;
+    content: (Variable | Content)[];
+}
 
-    interface Variable {
-        type: 'VARIABLE';
-        varName: string;
-        value: number;
-    }
+export interface Variable {
+    type: 'VARIABLE';
+    varName: string;
+    value: number;
 }
