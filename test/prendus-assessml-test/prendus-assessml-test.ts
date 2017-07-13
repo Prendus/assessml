f67473c07f0af8aa78c095c52f5992a3fe4c99f3
Jordan Last <jordan.michael.last@gmail.com>
2017-07-12 18:38:47 -0600

    preparing the test cases

--- test/prendus-assessml-test/prendus-assessml-test.ts
+++ test/prendus-assessml-test/prendus-assessml-test.ts
@@ -4,12 +4,24 @@ class PrendusAssessMLTest extends Polymer.Element {
     prepareTests(test) {
         const jsc = require('jsverify');
 
-        test('Just testing it out', [jsc.number, jsc.number], (arbNumber1, arbNumber2) => {
+        const arbAST = createArbAST();
+
+        test('The parse function should take an arbitrary AssessML document string and return a correct AssessML AST', [jsc.number, jsc.number], (arbNumber1, arbNumber2) => {
             return add(arbNumber1, arbNumber2) === arbNumber1 + arbNumber2;
         });
 
+        test('The compileToHTML function should take an arbitrary AssessML document string and return a correct HTML string', [], );
+
+        test('The compileToHTML function should take an arbitrary AssessML AST and return a correct HTML string', [], );
+
+        //TODO once getAstObjects gets more complicated, then you can test it
+
         function add(x, y) {
             return x + y;
+        }
+
+        function createArbAST() {
+
         }
     }
 }
